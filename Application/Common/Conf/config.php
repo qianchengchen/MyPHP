86701fa4204c9b07112c9e60f92521e4dee1e3a4
jochen01.chen <jochen01.chen@vipshop.com>
2016-04-18 14:48:28 +0800

    config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,13 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_TYPE'=>'mysql',  //数据库类型
+	'DB_HOST'=>'127.0.0.1',//数据库
+	'DB_NAME'=>'root' ,//数据库名称
+	'DB_PWD'=>'root',//数据库密码
+	'DB_PORT'=>'3306' ,//数据库端口
+	'DB_PARAMS'=>array(),
+	'DB_PREFIX'=>'hd_',
+	'DB_CHARSET'=>'utf8',
+	'DB_DEBUG'=>true,
 );
